cfd4dd954db7907e3576d29f825b18ad71c0833a
Jan David <jdno@jdno.dev>
2025-08-11 13:31:30 +0200

    Fix feature flags leaking into generated code
    When adding support for SeaORM, the implementation was refactored a few
    times to try different approaches. During one of these, the feature
    flags from this crate accidentally leaked into the generated code,
    causing warnings from Clippy when compiling the code.

--- src/url.rs
+++ src/url.rs
@@ -90,7 +90,6 @@ fn derives() -> proc_macro2::TokenStream {
 #[cfg(feature = "sea-orm")]
 fn sea_orm_trait_impls(ident: &Ident) -> proc_macro2::TokenStream {
     quote! {
-        #[cfg(feature = "sea-orm")]
         impl From<#ident> for sea_orm::Value {
             fn from(source: #ident) -> Self {
                 let string = source.get().to_string();
@@ -98,7 +97,6 @@ fn sea_orm_trait_impls(ident: &Ident) -> proc_macro2::TokenStream {
             }
         }
 
-        #[cfg(feature = "sea-orm")]
         impl sea_orm::TryGetable for #ident {
             fn try_get_by<I: sea_orm::ColIdx>(result: &sea_orm::QueryResult, index: I) -> Result<Self, sea_orm::TryGetError> {
                 let string = <String as sea_orm::TryGetable>::try_get_by(result, index)?;
@@ -108,7 +106,6 @@ fn sea_orm_trait_impls(ident: &Ident) -> proc_macro2::TokenStream {
             }
         }
 
-        #[cfg(feature = "sea-orm")]
         impl sea_orm::sea_query::ValueType for #ident {
             fn try_from(value: sea_orm::Value) -> Result<Self, sea_orm::sea_query::ValueTypeErr> {
                 let string = <String as sea_orm::sea_query::ValueType>::try_from(value)?;
@@ -129,7 +126,6 @@ fn sea_orm_trait_impls(ident: &Ident) -> proc_macro2::TokenStream {
             }
         }
 
-        #[cfg(feature = "sea-orm")]
         impl sea_orm::sea_query::Nullable for #ident {
             fn null() -> sea_orm::Value {
                 <String as sea_orm::sea_query::Nullable>::null()
